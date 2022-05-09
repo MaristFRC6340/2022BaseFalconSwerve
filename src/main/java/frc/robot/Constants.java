9855dad41b0c164a2e11ae6d00b2016b8d7e182e
nebomusic <michaudc@marist.com>
2022-05-09 14:25:46 -0400

    Test Update

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -79,6 +79,7 @@ public final class Constants {
         /* Module Specific Constants */
         /* Front Left Module - Module 0 */
         public static final class Mod0 {
+            // Updated for Marist Swerve2022
             public static final int driveMotorID = 7;
             public static final int angleMotorID = 8;
             public static final int canCoderID = 15;
