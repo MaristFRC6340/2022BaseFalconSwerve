8ae58b511e8a46fed3d90a234ea2ec7e16e50316
nebomusic <michaudc@marist.com>
2023-01-06 10:20:44 -0500

    auto experimentation

--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -26,15 +26,22 @@ public class exampleAuto extends SequentialCommandGroup {
                 .setKinematics(Constants.Swerve.swerveKinematics);
 
         // An example trajectory to follow.  All units in meters.
-        Trajectory exampleTrajectory =
+        // Trajectory exampleTrajectory =
+        //     TrajectoryGenerator.generateTrajectory(
+        //         // Start at the origin facing the +X direction
+        //         new Pose2d(0, 0, new Rotation2d(0)),
+        //         // Pass through these two interior waypoints, making an 's' curve path
+        //         List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        //         // End 3 meters straight ahead of where we started, facing forward
+        //         new Pose2d(3, 0, new Rotation2d(0)),
+        //         config);
+
+        Trajectory exampleTrajectory = 
             TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
                 new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                // End 3 meters straight ahead of where we started, facing forward
+                List.of(new Translation2d(1,0), new Translation2d(2,0)),
                 new Pose2d(3, 0, new Rotation2d(0)),
-                config);
+            config);
 
         var thetaController =
             new ProfiledPIDController(
